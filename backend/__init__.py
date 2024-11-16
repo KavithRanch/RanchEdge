54c9722ce53ddbfae72081842203768a62dbacab
KavithRanch <kavithranch@gmail.com>
2024-11-15 21:01:03 -0500

    1.1.2 Created two basic models (needs refinement over time)

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,19 @@
+from dotenv import load_dotenv
 import os
 from flask import Flask
-app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
+from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
+
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+from backend.models import Team, Player
 
 from backend import routes
+
+
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,27 @@
-T
+from backend import db
+from sqlalchemy import ForeignKey
+
+
+class Team(db.Model):
+    __tablename__ = "teams"
+
+    id = db.Column(db.Integer(), primary_key=True, nullable=False)
+    city = db.Column(db.String(length=20), nullable=False)
+    name = db.Column(db.String(length=20), nullable=False)
+    logo = db.Column(db.String, nullable=False)
+    conference = db.Column(db.String(length=7), nullable=False)
+    division = db.Column(db.String(length=12), nullable=False)
+    abbreviation = db.Column(db.String(length=3), nullable=False)
+
+
+class Player(db.Model):
+    __tablename__ = "players"
+
+    id = db.Column(db.Integer(), primary_key=True, nullable=False)
+    first_name = db.Column(db.String(length=30), nullable=False)
+    last_name = db.Column(db.String(length=30), nullable=False)
+    team_id = db.Column(db.Integer, ForeignKey('teams.id', ondelete="CASCADE"), nullable=False)
+    position = db.Column(db.String(length=2), nullable=False)
+    jersey_number = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.String(length=5), nullable=False)
+    weight = db.Column(db.String(length=3), nullable=False)
