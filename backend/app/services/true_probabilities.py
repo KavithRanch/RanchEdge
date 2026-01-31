ccf843cfb43d3de1157a4c55cdcf186152c072b7
KavithRanch <kavithranch@gmail.com>
2026-01-31 18:33:08 -0500

    0.4.3 Figured out logic for tru_prob database calls and updated python knowledge

--- backend/app/services/true_probabilities.py
+++ backend/app/services/true_probabilities.py
@@ -1,27 +1,52 @@
 from sqlalchemy import select
 from app.db.session import SessionLocal
 from app.models.price import Price
+from app.math.vig import remove_vig_two_way
+from app.math.odds import american_to_decimal, decimal_to_implied_probability
 
 
 def compute_true_probability_per_snapshot(odds_snapshot_id: int):
 
     with SessionLocal.begin() as session:
         # Pull all records allocated to one snapshot
-        stmt = (
-            select(Price)
-            .where(Price.snapshot_id == odds_snapshot_id)
-            .group_by(Price.market_id)
-        )
+        stmt = select(Price).where(Price.snapshot_id == odds_snapshot_id)
         prices = session.execute(stmt).scalars().all()
 
-        # TESTING CODE
-        print(prices)
-
+        groups = {}
         for p in prices:
-            print(f"Team: {p.outcome_name} @ odds: {p.american_odds}")
+            key = (p.market_id, p.sportsbook_id)
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(p)
 
-    # Go by market and remove the vig
-    # Avg out the probabilities for entries with the same market and outcome name
+        fair_prob_by_market = {}
+        for (m_id, _), price_group in groups.items():
+            print(f"{price_group[0].american_odds}, {price_group[1].american_odds}")
+            f_prob1, f_prob2 = remove_vig_two_way(
+                price_group[0].american_odds, price_group[1].american_odds
+            )
+
+            print(f"{f_prob1}, {f_prob2}, {f_prob1 + f_prob2}")
+
+        #     key1, key2 = (m_id, price_group[0].outcome_name), (
+        #         m_id,
+        #         price_group[1].outcome_name,
+        #     )
+        #     if key1 not in fair_prob_by_market:
+        #         fair_prob_by_market[key1]
+        #     if key2 not in fair_prob_by_market:
+        #         fair_prob_by_market[key2]
+
+        #     fair_prob_by_market[key1].append(f_prob1)
+        #     fair_prob_by_market[key2].append(f_prob1)
+
+        # for (m_id, outcome_name), f_prob in fair_prob_by_market.items():
+        #     print(
+        #         f"Market #: {m_id} | Team: {outcome_name} | # Fair Prob: {len(f_prob)}"
+        #     )
+
+    # Go by market and sportsbook to remove the vig from both outcomes
+    # Go by market and outcome
     # That is the true probability that we can store in a True Probability object
 
 
--- docs/python.md
+++ docs/python.md
@@ -1,6 +1,9 @@
 # (New) Things I Learned/Had Forgotten in Python
 
 ## Data Types
+* **Dictionaries** can be used to store key, value pairs and are usually declared with {}
+    * `dict.items()` -> [(key1, value1), (key2, value2)...]
+
 * **Iterable[T]** defines a capability that the object can be iterated over and get items of data type [T].
     * It is an abstraction for all iterable data types like lists, tuples, set, dict.
     * It does not guarantee that indexing, mutability and others are available.
